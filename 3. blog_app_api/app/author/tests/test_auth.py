9e6c3d1203881bdf502f4320997237625db61065
Ismail <ghvh8176@gmail.com>
2026-03-02 01:12:31 -0600

    refactor(blog): clean up tests to get client first

--- 3. blog_app_api/app/author/tests/test_auth.py
+++ 3. blog_app_api/app/author/tests/test_auth.py
@@ -3,16 +3,17 @@ from django.urls import reverse
 from rest_framework.test import APIClient
 from rest_framework import status
 
-AUTHOR_LIST_URL = reverse("authormodel-list")
-POST_LIST_URL = reverse("blogpostmodel-list")
 
 class AuthTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.author_list_url = reverse("authormodel-list")
+        self.post_list_url = reverse("blogpostmodel-list")
+
     def test_endpoint_author_requires_login(self):
-        client = APIClient()
-        res = client.get(AUTHOR_LIST_URL)
+        res = self.client.get(self.author_list_url)
         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_endpoint_post_requires_login(self):
-        client = APIClient()
-        res = client.get(POST_LIST_URL)
+        res = self.client.get(self.post_list_url)
         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
