e720bb6be675baf0b358e04800caf59b7fafd03f
Ismail <ghvh8176@gmail.com>
2026-03-05 01:57:44 -0600

    chore(blog): create reusable functions to reuse in the test (#6)

--- 3. blog_app_api/app/author/tests/test_posts.py
+++ 3. blog_app_api/app/author/tests/test_posts.py
@@ -2,7 +2,17 @@ from django.urls import reverse
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 
-from ..models import BlogPostModel
+from ..models import BlogPostModel, AuthorModel
 
 from rest_framework.test import APIClient
 from rest_framework import status
+
+BLOG_LIST_LINK = reverse("blogpostmodel-list")
+User = get_user_model()
+
+def create_user(usernane, password):
+    return User.objects.create(username=usernane, password=password)
+
+def create_author(user, name="Example", email="email@example.com"):
+    return AuthorModel.objects.create(user=user, name=name, email=email)
+
